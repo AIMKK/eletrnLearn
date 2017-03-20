af39bc4156e22ce110f98de98393dcacff2923d4
AIMKK <a_5210@126.com>
2017-03-20 14:03:39 +0800

    login ui

---
+++ main.js
@@ -0,0 +1,70 @@
+
+const electron = require('electron');
+// Module to control application life.
+const { app } = electron;
+// Module to create native browser window.
+const { BrowserWindow } = electron;
+// ipcMain
+const { ipcMain } = electron;
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win;
+
+function createWindow() {
+  // Create the browser window.
+  win = new BrowserWindow({width: 800, height: 600});
+
+  // and load the index.html of the app.
+  win.loadURL('file://' + __dirname + '/index.html');
+
+  // Open the DevTools.
+  win.webContents.openDevTools();
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null;
+  });
+  
+  // sending messages from the main process to the renderer process
+  win.webContents.on('did-finish-load', () => {
+    win.webContents.send('main-process-messages', 'webContents event "did-finish-load" called');
+  });
+  
+  // 监听Renderer Process的消息。
+  ipcMain.on('asynchronous-message', (event, arg) => {
+    // 返回消息
+    event.sender.send('asynchronous-reply', 'pong');
+    event.returnValue = 'asynchronous-return-value';
+  });
+
+  ipcMain.on('synchronous-message', (event, arg) => {
+    event.returnValue = 'pong';
+  });
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow();
+  }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
