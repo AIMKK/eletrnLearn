17e564d7782e657b6f2a182c476205cf7ef36e6c
AIMKK <a_5210@126.com>
2017-03-22 17:57:57 +0800

    frameless

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ let win;
 
 function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600});
+  win = new BrowserWindow({width: 800, height: 550, frame: false});
 
   // and load the index.html of the app.
   win.loadURL('file://' + __dirname + '/index.html');
