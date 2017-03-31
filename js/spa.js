d648192715948b2d8fc3397752a0f452b27188f3
AIMKK <a_5210@126.com>
2017-03-31 13:39:56 +0800

    sp ch4

--- js/spa.js
+++ js/spa.js
@@ -6,6 +6,9 @@
 /*global $, spa */
 var spa = (function () {
     var initModule = function ($container) {
+        // housekeeping here ...
+        // if we needed to configure the Shell,
+        // we would invoke spa.shell.configModule first
         spa.shell.initModule($container);
     };
     return { initModule: initModule };
