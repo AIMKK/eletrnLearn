18f50ebf4885b0295f52663ccec356631fa2f2df
AIMKK <a_5210@126.com>
2017-04-21 13:28:54 +0800

    .gitinnore file add

--- newTry1/main.js
+++ newTry1/main.js
@@ -1,17 +1,23 @@
 const electron = require('electron');
 const { app } = electron;
 const { BrowserWindow } = electron;
+const windowManager = require('electron-window-manager');
 var loginUI = require('./htmljs/loginUI');
 var mainUI;
 let win;
 
-app.on('ready', () => {   
-     var UI = require('./htmljs/mainUI');
-    UI(BrowserWindow);
-    loginUI = require('./htmljs/loginUI');
-    loginUI(BrowserWindow);
-});
+// app.on('ready', () => {   
+//      var UI = require('./htmljs/mainUI');
+//     UI(BrowserWindow);
+//     loginUI = require('./htmljs/loginUI');
+//     loginUI(BrowserWindow);
+// });
 
+app.on('ready', ()=> {
+    // windowManager.init({});
+    // // Open a window
+    windowManager.open('home', 'Welcome ...', './htmljs/mainUI');
+})
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit();
